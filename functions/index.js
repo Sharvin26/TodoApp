e8466e309aaca055f8a7dec825db4a0f6c559a2b
Sharvin Shah <sharvinshah51@gmail.com>
2020-03-30 20:27:49 +0530

    Completed API

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const app = require('express')();
+const auth = require('./util/auth');
  
 const {
     getAllTodos,
@@ -9,10 +10,24 @@ const {
     editTodo
 } = require('./APIs/todos')
 
-app.get('/todos', getAllTodos);
-app.get('/todo/:todoId', getOneTodo);
-app.post('/todo', postOneTodo);
-app.delete('/todo/:todoId', deleteTodo);
-app.put('/todo/:todoId', editTodo);
+const { 
+    loginUser,
+    signUpUser,
+    uploadProfilePhoto,
+    getUserDetail
+} = require('./APIs/users')
+
+// Todos
+app.get('/todos', auth, getAllTodos);
+app.get('/todo/:todoId', auth, getOneTodo);
+app.post('/todo',auth, postOneTodo);
+app.delete('/todo/:todoId',auth, deleteTodo);
+app.put('/todo/:todoId',auth, editTodo);
+
+// Users
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
+app.post('/user/image', auth ,uploadProfilePhoto);
+app.get('/user', auth, getUserDetail);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCS82wlCa7qfTE3ujk9qxuOTaSrA-vHfIM",
+    authDomain: "todoapp-655c1.firebaseapp.com",
+    databaseURL: "https://todoapp-655c1.firebaseio.com",
+    projectId: "todoapp-655c1",
+    storageBucket: "todoapp-655c1.appspot.com",
+    messagingSenderId: "908647968627",
+    appId: "1:908647968627:web:19d54bfdeb7a56c82edf06",
+    measurementId: "G-2PW3XNX0H9"
+};
